ef2842191c1edbef6122244687b17f6ded07cfbe
FranciscoTeo <francisco.teofilo16@gmail.com>
2025-11-22 20:18:21 +0000

    update 2

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import Inventory from "./Inventory";
 
 const rootElement = document.getElementById("inventory-root");
-if (!rootElement) throw new Error("Could not find inventory root element");
+if (!rootElement) throw new Error("Não encontrou #inventory-root");
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(<Inventory />);
